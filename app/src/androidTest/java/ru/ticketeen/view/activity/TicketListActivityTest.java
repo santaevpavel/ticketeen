b6e91f56b25a50292d33bb45bd48cbd5bb6b1b8d
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-09-02 18:12:08 +0300

    #2 test tickets list count

--- app/src/androidTest/java/ru/ticketeen/view/activity/TicketListActivityTest.java
+++ app/src/androidTest/java/ru/ticketeen/view/activity/TicketListActivityTest.java
@@ -4,12 +4,20 @@ package ru.ticketeen.view.activity;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.RecyclerView;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import ru.ticketeen.BaseSleepTest;
+import ru.ticketeen.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertFalse;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -20,8 +28,12 @@ public class TicketListActivityTest extends BaseSleepTest {
 
     @Test
     public void ticketListActivityTest() {
-        /*onView(withId(R.id.login_form))
-                .check(matches(isDisplayed()));*/
+        onView(withId(R.id.list))
+                .check(matches(isDisplayed()));
+
+        int numItems = ((RecyclerView) rule.getActivity()
+                .findViewById(R.id.list)).getAdapter().getItemCount();
+        assertFalse("List is empty", numItems != 0);
     }
 
 }
